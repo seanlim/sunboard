fc2f6e1df5477328d837d27a1856c6daefdee0d7
Yadunand Prem <yadunand@yadunut.com>
2023-05-14 13:36:14 +0800

    feat: add mock-moonboard

---
+++ mock-moonboard/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include "BLESerial.h"
+
+BLESerial bleSerial;
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+
+bool connected;
+
+void setup()
+{
+  Serial.begin(115200);
+  char bleName[] = "Sunboard";
+  bleSerial.begin(bleName);
+  Serial.println("-----------------");
+  Serial.println("Waiting for the mobile app to connect ...");
+  Serial.println("-----------------");
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  if (bleSerial.connected())
+
+  {
+    while (bleSerial.available())
+    {
+      char c = bleSerial.read();
+      Serial.print(c);
+      /* code */
+    }
+  }
+  // put your main code here, to run repeatedly:
+}
