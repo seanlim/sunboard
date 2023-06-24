e3bf9198f8a81d2a54c7658bb3a44f5b3ffae553
Sean Lim <seanlimdev856@gmail.com>
2023-06-24 21:53:40 +1000

    add constants file

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import BleManager, {Peripheral} from 'react-native-ble-manager';
 import {
   Button,
-  FlatList,
   ListRenderItemInfo,
   NativeEventEmitter,
   NativeModules,
@@ -11,13 +10,11 @@ import {
   View,
 } from 'react-native';
 import {useEffect, useState} from 'react';
+import { NORDIC_UART_SERVICE_UUID, RX_CHAR_UUID, SCAN_DURATION_SECONDS } from './constants';
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
-const NORDIC_UART_SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
-const RX_CHAR_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
-const SCAN_DURATION_SECONDS = 10;
 
 function App(): JSX.Element {
   const [peripherals, setPeripherals] = useState<Peripheral[]>([]);
